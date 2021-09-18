dc2404ed7ea51806abcbaf26fa3002e00142ba2b
LayintonDev <layintondev@gmail.com>
2021-09-18 14:50:53 +0100

    404 image

--- src/components/pages/404.jsx
+++ src/components/pages/404.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import Nav from "../layouts/Nav";
+import notFound from "../assets/404.jpg";
 
 const styles = {
   mainContainer: {
@@ -10,14 +11,31 @@ const styles = {
   },
   header: {
     margin: "auto",
+    textAlign: "center",
+  },
+  image: {
+    height: "350px",
+    width: "350px",
+  },
+  btnContainer: {
+    marginTop: "15px",
   },
 };
 const NotFound = () => (
   <>
     <Nav />
     <div style={styles.mainContainer}>
-      <h1 style={styles.header}>404 - Page Not Found!</h1>
-      <Link to="/">Go Home</Link>
+      {/* <h1 style={styles.header}>404 - Page Not Found!</h1> */}
+      <img style={styles.image} src={notFound} alt="404" />
+      <p style={styles.header}>
+        Hnmmm... Seems you have hit a wrong route. <br /> The page you're
+        looking for doesn't exists or has been removed.
+      </p>
+      <div style={styles.btnContainer} className="btn-box">
+        <Link className="theme-btn btn-style-two" to="/">
+          Go Home
+        </Link>
+      </div>
     </div>
   </>
 );
